548877f9a6ede8e35e4bb3b56066b002bee80772
AdrianNetto <adrian.netto73@gmail.com>
2025-04-28 10:42:17 -0300

    feat: operators

---
+++ 1/operadores.rb
@@ -0,0 +1,18 @@
+a, b = 1, 2
+
+if a > b
+    puts "a é maior do que b"
+else
+    puts "b é maior do que a"
+end
+
+
+puts "a é menor do que b" if a > b
+
+puts 1 + 1
+puts 2 - 1
+puts 2 * 2
+puts 2 / 2
+puts 10 % 5
+puts 11 % 5
+puts 2 ** 3
