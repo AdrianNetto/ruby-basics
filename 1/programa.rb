26682f1140432ea2b8779af596bf0a4bf983606e
AdrianNetto <adrian.netto73@gmail.com>
2025-04-28 10:19:39 -0300

    feat: variables, methods and scope

---
+++ 1/programa.rb
@@ -0,0 +1,25 @@
+# Este é um comentario no Ruby
+
+nome_completo = "John Doe"
+
+puts nome_completo
+
+nome_completo = nil # nil === null
+
+puts nome_completo.nil?
+
+puts 3.even?
+puts 3.odd?
+
+
+
+
+preco = 50
+
+def mudar_preco
+    preco = 100
+    puts preco
+end
+
+mudar_preco
+puts preco
